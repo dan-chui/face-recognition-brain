043850dfeefe6dadde94d3216d639fcb2f8d6f66
Dan Chui <dchui108@gmail.com>
2022-11-26 22:45:51 +0900

    First commit

---
+++ src/components/Logo/TiltComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Tilt from "react-parallax-tilt";
+import "./TiltComponent.css";
+  
+const TiltComponent = () => {
+    return (
+        <Tilt 
+        glareEnable={true} 
+        tiltMaxAngleX={30} 
+        tiltMaxAngleY={30} 
+        perspective={1000} 
+        glareColor={"rgb(255,0,0)"}>
+            <div className='tiltComponent'>
+                {/* Your Code Here */}
+            </div>
+        </Tilt>
+    )
+}
+  
+export default TiltComponent;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+      <div>
+      <div className="white f3">
+        {'Dan, your current Rank is...'}
+      </div>
+            <div className="white f1">
+        {'#3'}
+      </div>      
+      </div>
+  );
+}
+
+export default Rank;
